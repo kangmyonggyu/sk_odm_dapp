c2549e276abb16a5c2fdc8227043695dbb235125
Nate Kang <kang_mg@sk.com>
2020-11-17 00:06:48 +0900

    dd

--- router/contract/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const controller = require('./controller');
-
-router.get('/list', controller.list);
-router.post('/save_tx', controller.save_tx);
-//router.get('/get_server_info', controller.get_server_info);
-
-module.exports = router;
